ccd82ababae86327987d2e34f957d202c2fb9956
mm-khsv <khmari329@gmail.com>
2024-06-08 16:53:46 +0330

    Fix Code Style

--- Listeners/Settings/Email.php
+++ Listeners/Settings/Email.php
@@ -1,129 +1,135 @@
 <?php
+
 namespace packages\phpmailer_emailsender\Listeners\Settings;
-use \packages\base;
-use \packages\base\Translator;
-use \packages\base\Event;
-use \packages\base\Frontend\Theme;
-use \packages\base\Packages;
-use \packages\base\InputValidation;
-use \packages\email\Events\Senders;
-use \packages\email\Views\Settings\Senders as SenderViews;
-class Email{
-	public function senders_list(senders $senders){
-		$sender = new Senders\Sender("phpmailer");
-		$sender->setHandler(\packages\phpmailer_emailsender\API::class);
-		$sender->addInput(array(
-			'name' => 'phpmailer_smtp_enable',
-			'type' => 'bool',
-			'empty' => true
-		));
-		$sender->addInput(array(
-			'name' => 'phpmailer_smtp_hostname',
-			'type' => 'string',
-			'empty' => true,
-			'optional' => true
-		));
-		$sender->addInput(array(
-			'name' => 'phpmailer_smtp_port',
-			'type' => 'number',
-			'empty' => true,
-			'optional' => true
-		));
-		$sender->addInput(array(
-			'name' => 'phpmailer_smtp_username',
-			'type' => 'string',
-			'empty' => true,
-			'optional' => true
-		));
-		$sender->addInput(array(
-			'name' => 'phpmailer_smtp_password',
-			'type' => 'string',
-			'empty' => true,
-			'optional' => true
-		));
-		$sender->addInput(array(
-			'name' => 'phpmailer_smtp_secure',
-			'type' => 'string',
-			'empty' => true,
-			'optional' => true,
-			'values' => ['', 'tls', 'ssl'],
-		));
-		$sender->addField(array(
-			'type' => 'select',
-			'name' => 'phpmailer_smtp_enable',
-			'label' => t('settings.email.senders.phpmailer.smtp.enable'),
-			'options' => array(
-				array(
-					'value' => 1,
-					'title' => t('settings.email.senders.phpmailer.smtp.enable.yes')
-				),
-				array(
-					'value' => 0,
-					'title' => t('settings.email.senders.phpmailer.smtp.enable.no')
-				)
-			)
-		));
-		$sender->addField(array(
-			'name' => 'phpmailer_smtp_hostname',
-			'label' => t('settings.email.senders.phpmailer.smtp.hostname'),
-			'ltr' => true
-		));
-		$sender->addField(array(
-			'type' => 'number',
-			'name' => 'phpmailer_smtp_port',
-			'label' => t('settings.email.senders.phpmailer.smtp.port'),
-			'ltr' => true
-		));
-		$sender->addField(array(
-			'name' => 'phpmailer_smtp_username',
-			'label' => t('settings.email.senders.phpmailer.smtp.username'),
-			'ltr' => true
-		));
-		$sender->addField(array(
-			'type' => 'password',
-			'name' => 'phpmailer_smtp_password',
-			'label' => t('settings.email.senders.phpmailer.smtp.password'),
-			'ltr' => true
-		));
-		$sender->addField(array(
-			'type' => 'select',
-			'name' => 'phpmailer_smtp_secure',
-			'label' => t('settings.email.senders.phpmailer.smtp.secure'),
-			'ltr' => true,
-			'options' => array(
-				array(
-					'value' => '',
-					'title' => t('settings.email.senders.phpmailer.smtp.secure.none')
-				),
-				array(
-					'value' => 'ssl',
-					'title' => t('settings.email.senders.phpmailer.smtp.secure.ssl')
-				),
-				array(
-					'value' => 'tls',
-					'title' => t('settings.email.senders.phpmailer.smtp.secure.tls')
-				),
-			)
-		));
-		$sender->setController(__CLASS__.'@validate');
-		$senders->addSender($sender);
-	}
-	public function validate($inputs){
-		if($inputs['phpmailer_smtp_enable']){
-			foreach(array('hostname', 'port') as $input){
-				if(!isset($inputs['phpmailer_smtp_'.$input]) or !$inputs['phpmailer_smtp_'.$input]){
-					throw new InputValidation('phpmailer_smtp_'.$input);
-				}
-			}
-			if($inputs['phpmailer_smtp_port'] < 1 and $inputs['phpmailer_smtp_port'] > 65535){
-				throw new InputValidation('phpmailer_smtp_port');
-			}
-		}
-	}
-	public function senders_addAssets(Event $event){
-		$view = $event->getView();
-		if($view instanceof senderViews\add or $view instanceof senderViews\edit){
-			$view->addJSFile(Packages::package('phpmailer_emailsender')->url('frontend/assets/js/pages/senders.js'));
-		}
-	}
+
+use packages\base\Event;
+use packages\base\InputValidation;
+use packages\base\Packages;
+use packages\email\Events\Senders;
+use packages\email\Views\Settings\Senders as SenderViews;
+
+class Email
+{
+    public function senders_list(Senders $senders)
+    {
+        $sender = new Senders\Sender('phpmailer');
+        $sender->setHandler(\packages\phpmailer_emailsender\API::class);
+        $sender->addInput([
+            'name' => 'phpmailer_smtp_enable',
+            'type' => 'bool',
+            'empty' => true,
+        ]);
+        $sender->addInput([
+            'name' => 'phpmailer_smtp_hostname',
+            'type' => 'string',
+            'empty' => true,
+            'optional' => true,
+        ]);
+        $sender->addInput([
+            'name' => 'phpmailer_smtp_port',
+            'type' => 'number',
+            'empty' => true,
+            'optional' => true,
+        ]);
+        $sender->addInput([
+            'name' => 'phpmailer_smtp_username',
+            'type' => 'string',
+            'empty' => true,
+            'optional' => true,
+        ]);
+        $sender->addInput([
+            'name' => 'phpmailer_smtp_password',
+            'type' => 'string',
+            'empty' => true,
+            'optional' => true,
+        ]);
+        $sender->addInput([
+            'name' => 'phpmailer_smtp_secure',
+            'type' => 'string',
+            'empty' => true,
+            'optional' => true,
+            'values' => ['', 'tls', 'ssl'],
+        ]);
+        $sender->addField([
+            'type' => 'select',
+            'name' => 'phpmailer_smtp_enable',
+            'label' => t('settings.email.senders.phpmailer.smtp.enable'),
+            'options' => [
+                [
+                    'value' => 1,
+                    'title' => t('settings.email.senders.phpmailer.smtp.enable.yes'),
+                ],
+                [
+                    'value' => 0,
+                    'title' => t('settings.email.senders.phpmailer.smtp.enable.no'),
+                ],
+            ],
+        ]);
+        $sender->addField([
+            'name' => 'phpmailer_smtp_hostname',
+            'label' => t('settings.email.senders.phpmailer.smtp.hostname'),
+            'ltr' => true,
+        ]);
+        $sender->addField([
+            'type' => 'number',
+            'name' => 'phpmailer_smtp_port',
+            'label' => t('settings.email.senders.phpmailer.smtp.port'),
+            'ltr' => true,
+        ]);
+        $sender->addField([
+            'name' => 'phpmailer_smtp_username',
+            'label' => t('settings.email.senders.phpmailer.smtp.username'),
+            'ltr' => true,
+        ]);
+        $sender->addField([
+            'type' => 'password',
+            'name' => 'phpmailer_smtp_password',
+            'label' => t('settings.email.senders.phpmailer.smtp.password'),
+            'ltr' => true,
+        ]);
+        $sender->addField([
+            'type' => 'select',
+            'name' => 'phpmailer_smtp_secure',
+            'label' => t('settings.email.senders.phpmailer.smtp.secure'),
+            'ltr' => true,
+            'options' => [
+                [
+                    'value' => '',
+                    'title' => t('settings.email.senders.phpmailer.smtp.secure.none'),
+                ],
+                [
+                    'value' => 'ssl',
+                    'title' => t('settings.email.senders.phpmailer.smtp.secure.ssl'),
+                ],
+                [
+                    'value' => 'tls',
+                    'title' => t('settings.email.senders.phpmailer.smtp.secure.tls'),
+                ],
+            ],
+        ]);
+        $sender->setController(__CLASS__.'@validate');
+        $senders->addSender($sender);
+    }
+
+    public function validate($inputs)
+    {
+        if ($inputs['phpmailer_smtp_enable']) {
+            foreach (['hostname', 'port'] as $input) {
+                if (!isset($inputs['phpmailer_smtp_'.$input]) or !$inputs['phpmailer_smtp_'.$input]) {
+                    throw new InputValidation('phpmailer_smtp_'.$input);
+                }
+            }
+            if ($inputs['phpmailer_smtp_port'] < 1 and $inputs['phpmailer_smtp_port'] > 65535) {
+                throw new InputValidation('phpmailer_smtp_port');
+            }
+        }
+    }
+
+    public function senders_addAssets(Event $event)
+    {
+        $view = $event->getView();
+        if ($view instanceof SenderViews\add or $view instanceof SenderViews\edit) {
+            $view->addJSFile(Packages::package('phpmailer_emailsender')->url('frontend/assets/js/pages/senders.js'));
+        }
+    }
 }
--- libraries/API.php
+++ libraries/API.php
@@ -1,70 +1,78 @@
 <?php
+
 namespace packages\phpmailer_emailsender;
 
-use packages\email\{Sender, Sent};
-use packages\phpmailer\{PHPMailer, SMTP};
+use packages\email\Sender;
+use packages\email\Sent;
+use packages\phpmailer\PHPMailer;
 
-class API extends Sender\Handler {
+class API extends Sender\Handler
+{
+    /**
+     * @var Sender
+     */
+    private $sender;
 
-	/**
-	 * @var Sender
-	 */
-	private $sender;
+    public function __construct(Sender $sender)
+    {
+        $this->sender = $sender;
+    }
 
-	public function __construct(Sender $sender) {
-		$this->sender = $sender;
-	}
+    public function send(Sent $email)
+    {
+        $mailer = new PHPMailer();
+        if ($this->sender->param('phpmailer_smtp_enable')) {
+            $mailer->isSMTP();
+            $mailer->Hostname = $this->sender->param('phpmailer_smtp_hostname');
+            $mailer->Host = $this->sender->param('phpmailer_smtp_hostname');
+            $mailer->Port = $this->sender->param('phpmailer_smtp_port');
+            $secure = $this->sender->param('phpmailer_smtp_secure');
+            if (null === $secure or false === $secure) {
+                switch ($mailer->Port) {
+                    case 465:$secure = 'ssl';
+                        break;
+                    case 587:$secure = 'tls';
+                        break;
+                    default:$secure = '';
+                        break;
+                }
+            }
+            $mailer->SMTPSecure = $secure;
 
-	public function send(Sent $email) {
-		$mailer = new PHPMailer();
-		if ($this->sender->param('phpmailer_smtp_enable')) {
-			$mailer->isSMTP();
-			$mailer->Hostname = $this->sender->param('phpmailer_smtp_hostname');
-			$mailer->Host = $this->sender->param('phpmailer_smtp_hostname');
-			$mailer->Port = $this->sender->param('phpmailer_smtp_port');
-			$secure = $this->sender->param('phpmailer_smtp_secure');
-			if ($secure === null or $secure === false) {
-				switch($mailer->Port){
-					case(465):$secure = 'ssl';break;
-					case(587):$secure = 'tls';break;
-					default:$secure = '';break;
-				}
-			}
-			$mailer->SMTPSecure = $secure;
-			
-			$username = $this->sender->param('phpmailer_smtp_username');
-			$password = $this->sender->param('phpmailer_smtp_password');
-			if($username or $password){
-				$mailer->SMTPAuth = true;
-				$mailer->Username = $username;
-				$mailer->Password = $password;
-			}
-		}
-		$mailer->CharSet = 'utf-8';
-		$mailer->setFrom($email->sender_address->address, $email->sender_address->name);
-		$mailer->addAddress($email->receiver_address, $email->receiver_name);
-		$mailer->Subject = $email->subject;
+            $username = $this->sender->param('phpmailer_smtp_username');
+            $password = $this->sender->param('phpmailer_smtp_password');
+            if ($username or $password) {
+                $mailer->SMTPAuth = true;
+                $mailer->Username = $username;
+                $mailer->Password = $password;
+            }
+        }
+        $mailer->CharSet = 'utf-8';
+        $mailer->setFrom($email->sender_address->address, $email->sender_address->name);
+        $mailer->addAddress($email->receiver_address, $email->receiver_name);
+        $mailer->Subject = $email->subject;
 
-		$inlines = [];
+        $inlines = [];
 
-		if (preg_match_all("/cid:([\w,\-_\.]+)/", $email->html, $matches)) {
-			foreach ($matches[1] as $inline) {
-				if (!in_array($inline, $inlines)) {
-					$inlines[] = $inline;
-				}
-			}
-		}
+        if (preg_match_all("/cid:([\w,\-_\.]+)/", $email->html, $matches)) {
+            foreach ($matches[1] as $inline) {
+                if (!in_array($inline, $inlines)) {
+                    $inlines[] = $inline;
+                }
+            }
+        }
 
-		foreach ($email->attachments as $attachment) {
-			if (in_array($attachment->name, $inlines)) {
-				$mailer->addEmbeddedImage($attachment->file, $attachment->name);
-			} else {
-				$mailer->addAttachment($attachment->file, $attachment->name);
-			}
-		}
+        foreach ($email->attachments as $attachment) {
+            if (in_array($attachment->name, $inlines)) {
+                $mailer->addEmbeddedImage($attachment->file, $attachment->name);
+            } else {
+                $mailer->addAttachment($attachment->file, $attachment->name);
+            }
+        }
 
-		$mailer->msgHTML($email->html);
-		$mailer->AltBody = $email->text;
-		return $mailer->send() ? Sent::sent : Sent::failed;
-	}
+        $mailer->msgHTML($email->html);
+        $mailer->AltBody = $email->text;
+
+        return $mailer->send() ? Sent::sent : Sent::failed;
+    }
 }
